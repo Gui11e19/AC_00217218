455d73add65699de9005fabb41b8f3105833e627
Gui11e19 <guillermo.salazar4454@hotmail.com>
2021-04-19 22:39:37 -0600

    hot fix

--- tarea.asm
+++ tarea.asm
@@ -1,36 +1,53 @@
 
         org 	100h
 
-	    section	.text
+	section	.text
 
-;parte 1
-        mov	AX, "G"
-	    mov	BX, "A"
-	    mov	CX, "S"
-        mov DX, "B"
+        xor AX, AX
+        xor BX, BX
+        xor CX, CX
+        xor DX, DX
+        
 
-	    mov [200h], AX
-	    mov [201h], BX
-	    mov [202h], CX
-	    mov [203h], DX
+
+
+
+;parte 1        
+        xor AX, AX
+        mov AX, 47h
+        xor BX, BX
+	mov BX, 41h
+        xor CX, CX
+	mov CX, 53h
+        xor DX, DX
+        mov DX, 42h
+
+	mov [200h], AX
+	mov [201h], BX
+	mov [202h], CX
+	mov [203h], DX
 
 ;parte 2
 
+        
 
 ;copiar el valor de 200h a AX diraccionamiento directo
         mov AX, [200h] 
 
 ;copiar el valor de 201h a CX diraccionamiento indirecto por registro
+        
         mov BX, 201h 
-        mov	CX, [BX]
+        mov CX, [BX]
 
 ;copiar el valor de 202h a DX diraccionamiento indirecto base mas indice
+        xor BX, BX
         mov BX, 200h
         mov DI, 2h
-        mov	DX, [BX+DI]
+        mov DX, [BX+DI]
 
 ;copiar el valor de 203h a DI diraccionamiento indirecto relativo por registro
+        xor BX, BX
         mov BX, 3h
-        mov	DI, [BX+200h]
+        mov DI, [BX+200h]
 
-	    int	20h
+	int 20h
