01b9e52ca4a02831287c8877bb6000e21359abd0
Gui11e19 <40211527+Gui11e19@users.noreply.github.com>
2021-05-11 22:35:10 -0600

    Add files via upload

--- Tarea Labo #3/ejercicio2.asm
+++ Tarea Labo #3/ejercicio2.asm
@@ -1,27 +1,22 @@
 org 100h
 
+section .text
+        XOR AX, AX
+        MOV AX, 1d
+        MOV word CX, 5d
+        CMP CX, 0d
+        JE save
+        JMP fact
+fact:
+        MUL CX
+        LOOP fact
+save:
+        
+        MOV word [20Bh], AX         
+exit:
+        int 20h
+    
 
-.data:
-    res db 1
 
-.code:
-    MOV DX seg @data
-    mov ds, dx
 
-    mov cx, 5
 
-    ciclo:
-        mov al, res
-        mov bl, cl
-        mul bl
-        mov res, al 
-    loop ciclo
-
-    mov al, res
-    mov res, 20Bh
-
-    int 21h
-
-    .exit
-
-end
